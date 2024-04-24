bc1297452fa1527c43a5efaa9efc423ae2c1356c
KiwiKid <KiwiKid@users.noreply.github.com>
2024-04-25 08:40:23 +1200

    tweak release script

--- cut-release.sh
+++ cut-release.sh
@@ -7,10 +7,15 @@ new_version=$(npm version patch)
 # Extract only the version number without the leading 'v'
 new_version_number=${new_version:1}
 
-# Create a Git tag for the new version
-git tag -a $new_version -m "test release $new_version_number"
+# Check if the Git tag already exists
+if git rev-parse "$new_version" >/dev/null 2>&1; then
+    echo "Tag $new_version already exists."
+else
+    # Create a Git tag for the new version
+    git tag -a $new_version -m "Release $new_version_number"
 
-# Push the new tag to the remote repository
-git push origin $new_version
+    # Push the new tag to the remote repository
+    git push origin $new_version
 
-echo "Version bumped to $new_version_number, and tag pushed to GitHub."
+    echo "Version bumped to $new_version_number, and tag pushed to GitHub."
+fi
