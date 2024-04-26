ee1e676c0b8fb73aa3b733593501579f8dc087b9
KiwiKid <KiwiKid@users.noreply.github.com>
2024-04-26 18:45:02 +1200

    re-publish

---
+++ dist/nodes/get-entity-old/get-entity.js
@@ -0,0 +1,13 @@
+"use strict";
+const nodeInit = (RED) => {
+    function GetEntityNodeConstructor(config) {
+        RED.nodes.createNode(this, config);
+        this.on("input", (msg, send, done) => {
+            send(msg);
+            done();
+        });
+    }
+    RED.nodes.registerType("get-entity", GetEntityNodeConstructor);
+};
+module.exports = nodeInit;
+//# sourceMappingURL=get-entity.js.map
---
+++ dist/nodes/get-entity-old/shared/types.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EntityOption = void 0;
+var EntityOption;
+(function (EntityOption) {
+    EntityOption["Batteries"] = "batteries";
+    EntityOption["batteryChargeCycles"] = "battery_charge_cycles";
+    EntityOption["Chores"] = "chores";
+    EntityOption["Choreslog"] = "chores_log";
+    EntityOption["Equipment"] = "equipment";
+    EntityOption["Locations"] = "locations";
+    EntityOption["MealPlan"] = "meal_plan";
+    EntityOption["MealPlanSections"] = "meal_plan_sections";
+    EntityOption["ProductBarcodes"] = "product_barcodes";
+    EntityOption["ProductGroups"] = "product_groups";
+    EntityOption["Products"] = "products";
+    EntityOption["ProductsAveragePrice"] = "products_average_price";
+    EntityOption["ProductsLastPurchased"] = "products_last_purchased";
+    EntityOption["QuantityUnitConversions"] = "quantity_unit_conversions";
+    EntityOption["QuantityUnitConversionsResolved"] = "quantity_unit_conversions_resolved";
+    EntityOption["QuantityUnits"] = "quantity_units";
+    EntityOption["Recipes"] = "recipes";
+    EntityOption["RecipesNestings"] = "recipes_nestings";
+    EntityOption["RecipesPos"] = "recipes_pos";
+    EntityOption["RecipesPosResolved"] = "recipes_pos_resolved";
+    EntityOption["ShoppingList"] = "shopping_list";
+    EntityOption["ShoppingLists"] = "shopping_lists";
+    EntityOption["ShoppingLocations"] = "shopping_locations";
+    EntityOption["Stock"] = "stock";
+    EntityOption["StockCurrentLocations"] = "stock_current_locations";
+    EntityOption["StockLog"] = "stock_log";
+    EntityOption["TaskCategories"] = "task_categories";
+    EntityOption["Tasks"] = "tasks";
+    EntityOption["UserEntities"] = "userentities";
+    EntityOption["UserFields"] = "userfields";
+    EntityOption["UserObjects"] = "userobjects";
+})(EntityOption = exports.EntityOption || (exports.EntityOption = {}));
+//# sourceMappingURL=types.js.map
