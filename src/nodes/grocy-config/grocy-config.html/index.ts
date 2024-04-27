dd18b467fcd0b71c8973207847e2eb743da74317
KiwiKid <KiwiKid@users.noreply.github.com>
2024-04-28 10:57:35 +1200

    fix input field names

--- src/nodes/grocy-config/grocy-config.html/index.ts
+++ src/nodes/grocy-config/grocy-config.html/index.ts
@@ -8,12 +8,24 @@ RED.nodes.registerType<GrocyConfigEditorNodeProperties>("grocy-config", {
   defaults: {
     url: {
       value: "127.0.0.1:9283",
+      required: true
     },
     apiKey: {
       value: "",
+      required: true
     },
   },
   label: function () {
-    return this.name || "grocy config 1";
+    return this.name || this.url;
   },
+  oneditprepare: function () {
+    $('#node-input-url').val(this.url);
+    $('#node-input-api-key').val(this.apiKey);
+  },
+  oneditsave: function () {
+    const url = $('#node-input-url').val()?.toString() ?? ''
+    this.url = url;
+    const key = $('#node-input-api-key').val()?.toString() ?? ''
+    this.apiKey = key
+  }
 });
