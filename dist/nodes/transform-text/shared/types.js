7767d09390cd7913c92eafda9368f51f07502e94
KiwiKid <KiwiKid@users.noreply.github.com>
2024-05-06 23:06:19 +1200

    tidy up

--- dist/nodes/transform-text/shared/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TransformTextOperation = void 0;
-var TransformTextOperation;
-(function (TransformTextOperation) {
-    TransformTextOperation["UpperCase"] = "upper";
-    TransformTextOperation["LowerCase"] = "lower";
-})(TransformTextOperation = exports.TransformTextOperation || (exports.TransformTextOperation = {}));
-//# sourceMappingURL=types.js.map
