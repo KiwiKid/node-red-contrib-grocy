115f8568e069b0a4bd05f3977243a31df9412f57
KiwiKid <KiwiKid@users.noreply.github.com>
2024-05-10 20:35:43 +1200

    re-add config read

--- src/nodes/complete-task/complete-task.html/index.ts
+++ src/nodes/complete-task/complete-task.html/index.ts
@@ -20,21 +20,22 @@ RED.nodes.registerType<CompleteTaskEditorNodeProperties>("complete-task", {
   label: function () {
     return `${this.complete ? 'compelete' : 'undo'} grocy task #${this.task_id}`;
   },
- /* oneditprepare: function () {
+  oneditprepare: function () {
     $('#node-input-task-id').val(this.task_id);
     $('#node-input-complete').val(this.complete ? "true" : "false");
+    $('#node-input-server').val(this.server)?.toString() ?? '';
 
   },
- /* oneditsave: function () {
+ oneditsave: function () {
     // Access the flow context
     const server = $('#node-input-server').val()?.toString() ?? '';
     const task_id = $('#node-input-task-id').val()?.toString() ?? '';
     const complete = $('#node-input-complete').val()?.toString() ?? '';
 
     // Set values globally using RED.settings
-    //this.server = server
+    this.server = server
     this.task_id = +task_id;
     this.complete = complete == "true"
     // Notify user about the settings
- }*/
+ }
 });
